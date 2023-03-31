7abe92cde8e6655343daec790d3fcf75fac95570
Jean-Santoni <santonigs.2a@gmail.com>
2023-03-31 14:50:22 +0200

    Modification du Controlleur du gestionnaire Bancaire et du fichier XML

--- src/main/java/com/example/gestionnairebanque/Controleur.java
+++ src/main/java/com/example/gestionnairebanque/Controleur.java
@@ -1,9 +1,17 @@
 package com.example.gestionnairebanque;
 
+import com.example.gestionnairebanque.Model.GestionnaireBancaire;
+import com.example.gestionnairebanque.Model.Transaction;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class Controleur {
    @FXML
@@ -21,9 +29,24 @@ public class Controleur {
     @FXML
     private ObservableList<String> obsListCompte;
 
+    private GestionnaireBancaire gestionnaireBancaire;
+
 
     @FXML
     private void initialize() {
+     gestionnaireBancaire=new GestionnaireBancaire();
+     obsListCompte = FXCollections.observableArrayList();
+     ObservableList<String>listeCombo = FXCollections.observableArrayList();
+     listeCombo.add("Crédit");
+     listeCombo.add("Débit");
+     ComboCredit.setItems(listeCombo);
+     BtnEnregistrer.setOnMouseClicked(event -> actionEnregistrer(new ActionEvent() ));
+
+    }
+    private void actionEnregistrer(ActionEvent event){
+      obsListCompte.add("test");
+      ListVCompte.setItems(obsListCompte);
+
 
     }
 }
--- src/main/java/com/example/gestionnairebanque/Model/GestionnaireBancaire.java
+++ src/main/java/com/example/gestionnairebanque/Model/GestionnaireBancaire.java
@@ -1,4 +1,15 @@
 package com.example.gestionnairebanque.Model;
 
+import java.util.*;
+
 public class GestionnaireBancaire {
+    private ArrayList<Transaction> listTransaction;
+    private ArrayList<Taux> listTaux;
+    private Double soldeCompte;
+
+    public GestionnaireBancaire() {
+        this.listTransaction = new ArrayList<Transaction>() ;
+        this.listTaux = new ArrayList<Taux>();
+        this.soldeCompte = 0.0;
+    }
 }
