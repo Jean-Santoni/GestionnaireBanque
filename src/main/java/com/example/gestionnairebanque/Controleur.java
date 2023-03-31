262ec48f0bd3f8dad46154235499d744caad8a13
Jean-Santoni <santonigs.2a@gmail.com>
2023-03-31 14:10:38 +0200

    Modification du fichier XML ajout id et text au bouton et modification du Controleur

--- src/main/java/com/example/gestionnairebanque/Controleur.java
+++ src/main/java/com/example/gestionnairebanque/Controleur.java
@@ -1,14 +1,29 @@
 package com.example.gestionnairebanque;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class Controleur {
+   @FXML
+   private TextField TextFieldNom;
     @FXML
-    private Label welcomeText;
+    private TextField TextFieldMontant;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ComboBox ComboCredit;
+    @FXML
+    private Button BtnEnregistrer;
+
+    @FXML
+    private ListView ListVCompte;
+    @FXML
+    private ObservableList<String> obsListCompte;
+
+
+    @FXML
+    private void initialize() {
+
     }
 }
